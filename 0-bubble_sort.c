217e32dee8777a6a33cc82db6a52a7a9b0f175be
Ok-michael <okomichaelakpu2@gmail.com>
2024-01-21 20:24:52 +0100

    betty 0-bubble_sort.c

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -10,20 +10,20 @@
 void bubble_sort(int *array, size_t size)
 {
 	size_t k;
-	int tmp, sorted;
+	int tmp, sort;
 
 	if (array == NULL || size == 0)
 		return;
-	while(sorted == 0)
+	while (sort == 0)
 	{
-		sorted = 1;
+		sort = 1;
 		for (k = 0; k < size - 1; k++)
 		{
 			if (array[k] > array[k + 1])
 			{
-				sorted = 0;
+				sort = 0;
 				tmp = array[k];
-				array[k] = array[k+1];
+				array[k] = array[k + 1];
 				array[k + 1] = tmp;
 				print_array(array, size);
 			}
